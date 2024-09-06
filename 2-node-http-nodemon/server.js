fa71764b709410671d25f875f37aad973e0c2d00
hector <hectorportes80@gmail.com>
2024-09-06 10:51:03 -0400

    sl

---
+++ 2-node-http-nodemon/server.js
@@ -0,0 +1,19 @@
+// Importando o módulo/biblioteca http
+const { log } = require('console');
+const http = require('http');
+const { hostname } = require('os');
+
+// Definição de parâmetros do servidor
+const localhost = '127.0.0.1';
+const port = 3000;
+
+// Configuração inicial do servidor HTTP
+const server = http.createServer((requisicao, resposta) => {
+    resposta.statusCode = 200;
+    resposta.setHeader('Content-Type', 'text/plan; charset=utf-8');
+    resposta.write('Olá! Este é um servidor HTTP');
+    resposta.end();
+});
+
+// Executar o servidor
+server.listen(port, localhost, console.log('O servidor está rodando...'));
